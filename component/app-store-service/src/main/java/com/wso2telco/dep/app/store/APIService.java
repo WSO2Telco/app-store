1d22944688932da5cd0673e92e368c9f19322a23
Sumudu Jayasinghe <sumudu@wso2telco.com>
2018-02-07 20:51:04 +0530

    sample endpoint

--- component/app-store-service/src/main/java/com/wso2telco/dep/app/store/APIService.java
+++ component/app-store-service/src/main/java/com/wso2telco/dep/app/store/APIService.java
@@ -1,5 +1,17 @@
 package com.wso2telco.dep.app.store;
 
-public class APIService {
 
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+
+@Path("/applications")
+class APIService {
+
+    @GET
+    public Response assignApplication() {
+        String output = "Hi REST";
+		return Response.status(200).entity(output).build();
+    }
 }
+
+
