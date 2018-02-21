2d6b6c952307c8e1342a5c68be5cf98a5e3531f7
Sumudu Jayasinghe <sumudu@wso2telco.com>
2018-02-21 16:36:11 +0530

    APS-30  integrate swagger ui in to api console

--- component/app-store-ui/src/app/apis/components/api-console/api-console.component.ts
+++ component/app-store-ui/src/app/apis/components/api-console/api-console.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  AfterViewInit
+} from '@angular/core';
+
+import { SwaggerUIBundle, SwaggerUIStandalonePreset } from 'swagger-ui-dist';
 
 @Component({
   selector: 'store-api-console',
   templateUrl: './api-console.component.html',
   styleUrls: ['./api-console.component.scss']
 })
-export class ApiConsoleComponent implements OnInit {
+export class ApiConsoleComponent implements OnInit, AfterViewInit {
+  @ViewChild('swagger') container: ElementRef;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  ngAfterViewInit() {
+    const ui = SwaggerUIBundle({
+      url: 'https://localhost:9443/store/api-docs/admin/SwaggerPetstore/1.0.0',
+      domNode: this.container.nativeElement.querySelector('.swagger-container'),
+      deepLinking: true,
+      presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset],
+      plugins: [
+        SwaggerUIBundle.plugins.DownloadUrl,
+        () => {
+          return {
+            components: {
+              Topbar: () => null,
+              Info: () => null
+            }
+          };
+        }
+      ],
+      docExpansion: 'list',
+      jsonEditor: false,
+      defaultModelRendering: 'schema',
+      showRequestHeaders: true,
+      layout: 'StandaloneLayout'
+    });
   }
-
 }
