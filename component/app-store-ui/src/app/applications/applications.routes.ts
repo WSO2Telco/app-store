9b35fed736c34019fcb92e21412688e5f3ed2dfe
Gayantha Samaradiwakara <gayantha.samaradiwakara@apigate.com>
2019-08-13 08:32:02 +0530

    INTGW-657 : Application module cleanup

--- component/app-store-ui/src/app/applications/applications.routes.ts
+++ component/app-store-ui/src/app/applications/applications.routes.ts
@@ -2,10 +2,6 @@ import { SearchApplicationsComponent } from './components/search-applications/se
 import { RouterModule } from '@angular/router';
 import { CreateApplicationComponent } from './components/create-application/create-application.component';
 import { ApplicationDetailMainComponent } from './components/application-detail-main/application-detail-main.component';
-import { ApplicationProductionKeysComponent } from './components/application-production-keys/application-production-keys.component';
-import { ApplicationSandboxKeysComponent } from './components/application-sandbox-keys/application-sandbox-keys.component';
-import { ApplicationSubscriptionsComponent } from './components/application-subscriptions/application-subscriptions.component';
-import { ApplicationOverviewComponent } from './components/application-overview/application-overview.component';
 
 const routes = [
     {
@@ -23,14 +19,7 @@ const routes = [
     },
     {
         path: ':appId/:tab',
-        component: ApplicationDetailMainComponent,
-        // children: [
-        //     { path: '', redirectTo: 'overview', pathMatch: 'full' },
-        //     { path: 'overview', component: ApplicationOverviewComponent },
-        //     { path: 'production-keys', component: ApplicationProductionKeysComponent  },
-        //     { path: 'sandbox-keys', component: ApplicationSandboxKeysComponent },
-        //     { path: 'subscriptions', component: ApplicationSubscriptionsComponent }
-        // ]
+        component: ApplicationDetailMainComponent
     }
 ];
 
--- component/app-store-ui/src/app/applications/components/application-detail-main/application-detail-main.component.ts
+++ component/app-store-ui/src/app/applications/components/application-detail-main/application-detail-main.component.ts
@@ -1,10 +1,15 @@
 import { Component, OnInit } from "@angular/core";
-import { Router, ActivatedRoute } from "@angular/router";
+import { ActivatedRoute } from "@angular/router";
 import { Location } from '@angular/common';
+import { MatTableDataSource, MatDialog } from "@angular/material";
 import { Store } from '@ngrx/store';
 
 import { TabTile } from "../../applications.data.models";
 import { AppState } from '../../../app.data.models';
+import { Subscription } from "../../applications.data.models";
+import { NotificationService } from "../../../shared/services/notification.service";
+import { ConfirmDialogComponent } from "../../../commons/components/confirm-dialog/confirm-dialog.component";
+
 import * as applicationsActions from '../../applications.actions';
 
 @Component({
@@ -16,11 +21,15 @@ export class ApplicationDetailMainComponent implements OnInit {
   appId : number;
   activatedTab : string;
 
+  //Subscription
+  public datasource: MatTableDataSource<Subscription> = new MatTableDataSource();
+
   constructor(
-    private router: Router, 
     private route: ActivatedRoute, 
     private store: Store<AppState>,
-    private location: Location
+    private location: Location,
+    public dialog: MatDialog,
+    private notification: NotificationService
   ) {}
 
   ngOnInit() {
@@ -31,11 +40,49 @@ export class ApplicationDetailMainComponent implements OnInit {
         new applicationsActions.SetSelectedApplicationsAction(this.appId)
       );
     })
+
+    //subscription
+    this.store
+      .select(s => s.applications.appSubscriptions)
+      .subscribe(res => (this.datasource.data = res));
+
+    this.store
+      .select(s => s.applications.selectedApplication)
+      .subscribe(app => {
+        if (app) {
+          this.store.dispatch(
+            new applicationsActions.GetApplicationSubscriptionsAction(app)
+          );
+        }
+    });
   }
 
   switchTab(tab){
     //  this.router.navigate([`/applications/${this.appId}/${tile.route}`]);
     this.activatedTab = tab;
      this.location.replaceState(`/applications/${this.appId}/${tab}`);
+  }
+
+  //subscription
+  onAction(sub, action) {
+    if (action === "unsubscribe") {
+      const ref = this.dialog.open(ConfirmDialogComponent, {
+        data: {
+          title: "Confirm Unsubscribe",
+          message:
+            'Are you sure you want to unsubscribe from "' +
+            sub.apiName +
+            " - " +
+            sub.apiVersion +
+            '"?'
+        }
+      });
+
+      ref.afterClosed().subscribe(res => {
+        if (res) {
+          this.notification.success("Subscription successfully deleted");
+        }
+      });
+    }
   }
 }
