484fc9f16a86d32ed2ff3f292e42de3e142c4d2c
Gayantha Samaradiwakara <gayantha.samaradiwakara@apigate.com>
2019-08-23 17:37:24 +0530

    INTGW-657 : Home page (dashboard) updates

--- component/app-store-ui/src/app/home/components/dashboard/dashboard.component.ts
+++ component/app-store-ui/src/app/home/components/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild } from "@angular/core";
 import { ThumbnailParam } from "../../home.data.models";
+import { IImage } from 'ng-simple-slideshow';
 
 @Component({
   selector: "store-dashboard",
@@ -13,6 +14,18 @@ export class DashboardComponent implements OnInit {
     new ThumbnailParam('Forum','/forum','forum', 'forum'),
     new ThumbnailParam('Statistics','/statistics','stat', 'insert_chart')
   ];
+
+  imageUrlArray: (string | IImage)[] = [
+    { url: 'assets/slides/1.png', caption: 'Apigate', href: '#config' },
+    { url: 'assets/slides/2.png', caption: 'Apigate', href: '#config' },
+    { url: 'assets/slides/3.png', caption: 'Apigate', href: '#config' },
+    { url: 'assets/slides/4.png', caption: 'Apigate', href: '#config' },
+    { url: 'assets/slides/5.png', caption: 'Apigate', href: '#config' },
+    { url: 'assets/slides/6.png', caption: 'Apigate', href: '#config' },
+    // { url: 'https://cdn.vox-cdn.com/uploads/chorus_asset/file/9278671/jbareham_170917_2000_0124.jpg', clickAction: () => alert('custom click function') },
+  ];
+
+  // @ViewChild('slideshow') slideshow: any;
 
   constructor() {}
 
