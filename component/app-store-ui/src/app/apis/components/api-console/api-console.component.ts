d89818caa478872703c5d0159c7707c11927b557
Sahan Kariyawasam <sahan@wso2telco.com>
2019-08-27 16:22:05 +0530

    Fix conflicts

--- component/app-store-ui/src/app/apis/components/api-console/api-console.component.ts
+++ component/app-store-ui/src/app/apis/components/api-console/api-console.component.ts
@@ -3,10 +3,13 @@ import {
   OnInit,
   ViewChild,
   ElementRef,
-  AfterViewInit
+  AfterViewInit,
+  Input
 } from '@angular/core';
 
 import { SwaggerUIBundle, SwaggerUIStandalonePreset } from 'swagger-ui-dist';
+import { ApiOverview, ApiSummery } from '../../apis.models';
+import { ApiEndpoints } from '../../../config/api.endpoints';
 
 @Component({
   selector: 'store-api-console',
@@ -14,15 +17,17 @@ import { SwaggerUIBundle, SwaggerUIStandalonePreset } from 'swagger-ui-dist';
   styleUrls: ['./api-console.component.scss']
 })
 export class ApiConsoleComponent implements OnInit, AfterViewInit {
-  @ViewChild('swagger', {static:true}) container: ElementRef;
+  @ViewChild('swagger', { static: true }) container: ElementRef;
+  @Input() public apiOverview: ApiSummery;
+  apiPrefix = ApiEndpoints.apiContext;
+  constructor() { }
 
-  constructor() {}
-
-  ngOnInit() {}
+  ngOnInit() {
+  }
 
   ngAfterViewInit() {
     const ui = SwaggerUIBundle({
-      url: 'assets/files/swagger.json',
+      url: this.apiPrefix + '/apis/' + this.apiOverview.id + '/swagger',
       domNode: this.container.nativeElement.querySelector('.swagger-container'),
       presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset],
       plugins: [
