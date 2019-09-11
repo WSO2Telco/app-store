efe6a36141cd1d02659975fcefe10cee25063940
Sahan Kariyawasam <sahan@wso2telco.com>
2019-09-11 11:52:07 +0530

    Adding new authentication model

--- component/app-store-ui/src/app/authentication/authentication.models.ts
+++ component/app-store-ui/src/app/authentication/authentication.models.ts
@@ -1,7 +1,9 @@
 export interface AuthState {
     loginData: LoginResponseData;
     menuData: IMenuItem[];
-    lastAuthRequiredRoute:string;
+    lastAuthRequiredRoute: string;
+    registeredAppData: RegClientData;
+    tokenDetails:TokenData;
 }
 
 export interface IMenuItem {
@@ -12,6 +14,42 @@ export interface IMenuItem {
 export class LoginMenuAction {
     constructor(public type: LoginMenuActionTypes) {
     }
+}
+
+
+export class ClientRegParam {
+    callbackUrl: string;
+    clientName: string;
+    owner: string;
+    grantType: string;
+    saasApp: boolean;
+}
+
+export class TokenGenerationParam {
+    password: string;
+    username: string;
+    grant_type: string;
+    scope: string;
+}
+
+export class TokenData {
+    access_token: string;
+    refresh_token: string;
+    scope: string;
+    token_type: string;
+    expires_in: number
+}
+
+export interface RegClientData {
+    clientId: string;
+    clientName: string;
+    callBackURL: string;
+    clientSecret: string;
+    isSaasApplication: boolean;
+    appOwner: string;
+    jsonString: string;
+    jsonAppAttribute: string;
+    tokenType: string;
 }
 
 export enum LoginMenuActionTypes {
@@ -28,8 +66,6 @@ export class LoginFormData {
 
 export class LoginResponseData {
     constructor(
-        public username: string,
-        public permissions: any,
         public error: boolean = false,
         public message: string = '') { }
 }
