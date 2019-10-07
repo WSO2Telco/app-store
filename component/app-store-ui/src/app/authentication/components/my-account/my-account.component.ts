3baa26f4d88954c4b3dd824398cecf66c818fe1a
Sahan Kariyawasam <sahan@wso2telco.com>
2019-10-07 20:47:45 +0530

    Adding Update password UI

--- component/app-store-ui/src/app/authentication/components/my-account/my-account.component.ts
+++ component/app-store-ui/src/app/authentication/components/my-account/my-account.component.ts
@@ -2,9 +2,19 @@ import { Component, OnInit, ViewChild } from "@angular/core";
 import { ResetPasswordParam } from "../../authentication.models";
 import { AppState } from "../../../app.data.models";
 import { Store } from "@ngrx/store";
-import { Actions } from '@ngrx/effects';
-import { NgForm } from '@angular/forms';
-import { ChangeUserPwAction } from '../../authentication.actions';
+import { Actions, ofType } from '@ngrx/effects';
+import { NgForm, FormGroup, FormBuilder, Validators, FormControl, FormGroupDirective } from '@angular/forms';
+import { ChangeUserPwAction, ChangeUserPwSuccessAction } from '../../authentication.actions';
+import { ErrorStateMatcher } from '@angular/material/core';
+
+export class UpdatePwErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const invalidCtrl = !!(control && control.invalid && control.parent.dirty);
+    const invalidParent = !!(control && control.parent && control.parent.invalid && control.parent.dirty);
+
+    return (invalidCtrl || invalidParent);
+  }
+}
 
 @Component({
   selector: "store-my-account",
@@ -12,19 +22,47 @@ import { ChangeUserPwAction } from '../../authentication.actions';
   styleUrls: ["./my-account.component.scss"]
 })
 export class MyAccountComponent implements OnInit {
-  @ViewChild(NgForm, {static:false}) changePwForm: NgForm;
-  
+  //@ViewChild(NgForm, { static: false }) changePwForm: NgForm;
+  updatePwForm: FormGroup;
+  matcher = new UpdatePwErrorStateMatcher();
   param: ResetPasswordParam;
-  confirmPassword:string;
+  confirmPassword: string;
 
-  constructor(private store: Store<AppState>,private actions: Actions) {}
+  constructor(private store: Store<AppState>, private actions: Actions,private formBuilder: FormBuilder) {
+    this.updatePwForm = this.formBuilder.group({
+      currentpassword: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+      confirmPassword: ['']
+    }, { validator: this.checkPasswords });
+   }
 
   ngOnInit() {
     this.param = new ResetPasswordParam();
     // this.actions.ofType(authActions.CHANGE_USER_PW_SUCCESS).subscribe((r)=>this.changePwForm.reset());
   }
 
-  onChangePw(form) {
-    this.store.dispatch(ChangeUserPwAction({"payload" : this.param}));
+  checkPasswords(group: FormGroup) { // here we have the 'passwords' group
+  let pass = group.controls.password.value;
+  let confirmPass = group.controls.confirmPassword.value;
+
+  return pass === confirmPass ? null : { notSame: true }
+}
+
+  onChangePw(form:any, formDirective: FormGroupDirective) {
+    if (this.updatePwForm.valid) {
+      this.param.currentPassword = this.updatePwForm.value.currentpassword;
+      this.param.newPassword = this.updatePwForm.value.password;
+      console.log(this.param)
+      this.store.dispatch(ChangeUserPwAction({ "payload": this.param }));
+
+      this.actions.pipe(ofType(ChangeUserPwSuccessAction)).subscribe(p => {
+      /*   this.submitted = false;
+        formDirective.resetForm(); */
+        formDirective.resetForm();
+        this.updatePwForm.reset();
+      })
+    } else { 
+      console.log('error');
+    }
   }
 }
