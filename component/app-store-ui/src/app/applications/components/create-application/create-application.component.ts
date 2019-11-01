9309c6ac3b1b8636e696bed2348c8455d6e52917
Sahan Kariyawasam <sahan@wso2telco.com>
2019-11-01 17:48:07 +0530

    Update application feature

--- component/app-store-ui/src/app/applications/components/create-application/create-application.component.ts
+++ component/app-store-ui/src/app/applications/components/create-application/create-application.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { CreateApplicationParam } from '../../applications.data.models';
+import { CreateApplicationParam, ApplicationDetails } from '../../applications.data.models';
 
 //Breadcrumbs
 import * as globalActions from "../../../app.actions";
@@ -11,6 +11,8 @@ import { FormGroup, FormBuilder, Validators, FormGroupDirective } from '@angular
 
 import * as applicationActions from "../../../applications/applications.actions";
 import { Actions, ofType } from '@ngrx/effects';
+import { ActivatedRoute } from '@angular/router';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'store-create-application',
@@ -20,47 +22,74 @@ import { Actions, ofType } from '@ngrx/effects';
 export class CreateApplicationComponent implements OnInit {
 
   application: CreateApplicationParam = new CreateApplicationParam();
-
+  appData: ApplicationDetails;
   formCreateApp: FormGroup;
   submitted = false;
   public appDescription: string;
   public appName: string;
-
   constructor(
     private store: Store<AppState>,
     private titleService: Title,
     private fb: FormBuilder,
     private actions$: Actions,
-  ) { }
-
-  ngOnInit() {
-    this.store.dispatch(globalActions.SetBreadcrumbAction({ payload: [new BreadcrumbItem("Applications")] }));
-    this.titleService.setTitle("Create New App | Apigate API Store");
-
+    private route: ActivatedRoute,
+  ) {
     this.formCreateApp = this.fb.group({
       appName: ['', [Validators.required, Validators.maxLength(70), Validators.pattern("[a-zA-Z\s]+$")]],
       appDescription: ['', Validators.required]
     });
 
+  }
 
+  ngOnInit() {
+    this.store.dispatch(globalActions.SetBreadcrumbAction({ payload: [new BreadcrumbItem("Applications")] }));
+    this.titleService.setTitle("Create New App | Apigate API Store");
+
+    this.route.params.subscribe(params => {
+      let appId = params['appId'];
+      if (appId != null) {
+        this.store.dispatch(
+          applicationActions.GetApplicationDetailsAction({ "payload": appId })
+        );
+
+        this.actions$.pipe(ofType(applicationActions.GetApplicationDetailsSuccessAction)).pipe(take(1)).subscribe(p => {
+          if (p) {
+            this.appData = p.payload
+            this.formCreateApp.controls.appName.setValue(this.appData.name);
+            this.formCreateApp.controls.appDescription.setValue(this.appData.description);
+          }
+        })
+      }
+    })
   }
 
   get f() { return this.formCreateApp.controls; }
 
   onSubmit(fData: any, formDirective: FormGroupDirective) {
     this.submitted = true;
-    if (this.formCreateApp.invalid) {
+    if (!this.formCreateApp.valid) {
       return;
     } else {
-      this.application.name = this.appName;
-      this.application.description = this.appDescription;
-      this.store.dispatch(applicationActions.CreateApplicationsAction({ "payload": this.application }))
+      this.application.name = this.formCreateApp.value.appName;
+      this.application.description = this.formCreateApp.value.appDescription;
 
-      this.actions$.pipe(ofType(applicationActions.CreateApplicationSuccessAction)).subscribe(p => {
-        this.submitted = false;
-        formDirective.resetForm();
-        this.formCreateApp.reset();
-      })
+      if (!this.appData) {
+        this.store.dispatch(applicationActions.CreateApplicationsAction({ "payload": this.application }))
+
+        this.actions$.pipe(ofType(applicationActions.CreateApplicationSuccessAction)).subscribe(p => {
+          this.submitted = false;
+          formDirective.resetForm();
+          this.formCreateApp.reset();
+        })
+      } else {
+        this.store.dispatch(applicationActions.UpdateApplicationsAction({ "appId": this.appData.applicationId, "payload": this.application }))
+
+        this.actions$.pipe(ofType(applicationActions.UpdateApplicationSuccessAction)).subscribe(p => {
+          this.submitted = false;
+          formDirective.resetForm();
+          this.formCreateApp.reset();
+        })
+      }
     }
   }
 
