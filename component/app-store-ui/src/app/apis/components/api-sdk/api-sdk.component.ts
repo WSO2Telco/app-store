60b3981dfaadad1a3cbc235ab292ad3937844a60
Sahan Kariyawasam <sahan@wso2telco.com>
2019-11-14 09:46:52 +0530

    Adding SDK component

---
+++ component/app-store-ui/src/app/apis/components/api-sdk/api-sdk.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'store-api-sdk',
+  templateUrl: './api-sdk.component.html',
+  styleUrls: ['./api-sdk.component.scss']
+})
+export class ApiSdkComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+
+  downloadAndriod(){
+    
+  }
+
+}
