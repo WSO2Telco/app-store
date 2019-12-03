0e0c420130509e0fccd3c9afe5cb546d4db9739b
Sahan Kariyawasam <sahan@wso2telco.com>
2019-12-03 16:37:32 +0530

    Adding new subscription to selected api

--- component/app-store-ui/src/app/apis/components/api-app-subscriptions/api-app-subscriptions.component.ts
+++ component/app-store-ui/src/app/apis/components/api-app-subscriptions/api-app-subscriptions.component.ts
@@ -3,8 +3,13 @@ import { Store } from '@ngrx/store';
 import { MatDialog, MatTableDataSource } from '@angular/material';
 import { AppState } from '../../../app.data.models';
 import { NotificationService } from "../../../shared/services/notification.service";
-import { Subscription } from "../../../applications/applications.data.models";
-import { GetUserApplicationsAction, GetUserSubscriptionsAction } from '../../apis.actions';
+import { Subscription, GetApplicationsParam } from "../../../applications/applications.data.models";
+import { GetUserApplicationsAction, GetUserSubscriptionsAction, DoNewSubscribeAction, DoNewSubscribeSuccessAction } from '../../apis.actions';
+import { ActivatedRoute } from '@angular/router';
+import { Actions, ofType } from '@ngrx/effects';
+import { GetAllApplicationsAction } from '../../../applications/applications.actions';
+import { ActionDialogComponent } from '../../../commons/components/action-dialog/action-dialog.component';
+import { AddNewSubsParam } from '../../apis.models';
 
 @Component({
   selector: 'store-api-app-subscriptions',
@@ -13,29 +18,70 @@ import { GetUserApplicationsAction, GetUserSubscriptionsAction } from '../../api
 })
 export class ApiAppSubscriptionsComponent implements OnInit {
   public datasource: MatTableDataSource<Subscription> = new MatTableDataSource();
+  api_id: string;
+  appResult;
 
   constructor(
     private store: Store<AppState>,
     public dialog: MatDialog,
-    private notification: NotificationService
+    private route: ActivatedRoute,
+    private notification: NotificationService,
+    private actions$: Actions
   ) { }
 
   ngOnInit() {
+
+    this.route.params.subscribe(p => {
+      this.api_id = p['apiId'];
+    })
     this.store
-      .select(s => s.applications.appSubscriptions)
+      .select(s => s.apis.apiSubscriptions)
       .subscribe(res => {
-        if(res && res.list) this.datasource.data = res.list
+        if (res && res.list) this.datasource.data = res.list
       });
 
     this.store
       .select(s => s.apis.selectedApi)
       .subscribe(app => {
         if (app) {
-          this.store.dispatch(GetUserSubscriptionsAction({ "payload" : app.id}));
+          this.store.dispatch(GetUserSubscriptionsAction({ "payload": this.api_id }));
         }
       });
+
+    this.store
+      .select(s => s.apis.availableApp)
+      .subscribe(apps => {
+        this.appResult = apps.list;
+
+      });
+
   }
 
-  newSubscription() { }
+  openDialog(event: any) {
+    var mapResult = this.appResult.map(appArr => ({ value: appArr.applicationId, viewValue: appArr.name }));
+    event.stopPropagation();
+    const dialogRef = this.dialog.open(ActionDialogComponent, {
+      data: {
+        appList: mapResult,
+        buttonText: {
+          ok: 'Save',
+          cancel: 'No'
+        }
+      }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.store.dispatch(DoNewSubscribeAction({ "payload": new AddNewSubsParam('Unlimited', this.api_id, result) }));
+
+        this.actions$.pipe(ofType(DoNewSubscribeSuccessAction)).subscribe(p => {
+          if (p) {
+            this.store.dispatch(GetUserSubscriptionsAction({ "payload": this.api_id }));
+          }
+        })
+      }
+    }
+    );
+  }
 
 }
