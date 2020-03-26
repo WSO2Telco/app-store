9a186a8ed4ef22c73dc598f4f4920afb1face0aa
Gayantha Samaradiwakara <gayantha.samaradiwakara@apigate.com>
2020-03-26 20:38:55 +0530

    [INTGW-1013] subscription dialog themeing fix

--- component/app-store-ui/src/app/commons/components/action-dialog/action-dialog.component.ts
+++ component/app-store-ui/src/app/commons/components/action-dialog/action-dialog.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { ActionDialogParam } from '../../commons.data.models';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../../app.data.models';
 
 @Component({
   selector: 'store-action-dialog',
@@ -13,7 +15,12 @@ export class ActionDialogComponent implements OnInit {
   
   constructor(
     public dialogRef: MatDialogRef<ActionDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: ActionDialogParam) { }
+    @Inject(MAT_DIALOG_DATA) public data: ActionDialogParam,
+    private store: Store<AppState>) {
+      this.store.select((s) => s.global.layout.appTheme).subscribe((theme) => {
+        dialogRef.addPanelClass(theme);
+      });
+     }
 
   ngOnInit() {
   }
